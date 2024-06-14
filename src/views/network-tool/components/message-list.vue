b14d7fc87740d7f6e33cde93b0a31e0caee63cc1
linzili <87574485+linzili@users.noreply.github.com>
2024-06-14 21:04:19 +0800

    ✨feat(物联工具): 在物联工具消息列表中，设置不同的背景颜色来区分接受和发送的消息 (#21)
    - 为接收的消息设置一种背景颜色和符号。
    - 为发送的消息设置另一种背景颜色和符号。
    - 添加提示信息帮助用户使用
    
    Closes #18

--- src/views/network-tool/components/message-list.vue
+++ src/views/network-tool/components/message-list.vue
@@ -29,14 +29,15 @@ function handleClear() {
 <template>
   <a-card class="message-container w-full pt-4">
     <div ref="messageContainer" class="h-[calc(100%_-_2.5rem)] overflow-auto">
-      <div v-for="item in networkStore.currentMessageList" :key="item.time" class="mb-4" :style="{ maxWidth: '90%' }">
-        <div class="inline-block p-2 rounded-lg" :style="{ backgroundColor: token.colorBgLayout, maxWidth: '90%' }">
+      <div v-for="item in networkStore.currentMessageList" :key="item.time" class="mb-4 w-11/12">
+        <div class="inline-block p-2 rounded-lg" :style="{ backgroundColor: item.type === 'receive' ? token.colorInfoBg : token.colorFillTertiary }">
           <a-row :gutter="16">
             <a-col v-if="showTime">
               <a-typography-text :style="{ color: token.colorPrimary }"> [{{ item.time }}] </a-typography-text>
             </a-col>
             <a-col>
-              <a-typography-text strong> {{ item.type === 'receive' ? '<<' : '>>' }} </a-typography-text>
+              <FallOutlined v-if="item.type === 'receive'" />
+              <RiseOutlined v-else />
             </a-col>
             <a-col>
               <a-typography-text class="whitespace-pre-wrap">
@@ -48,12 +49,16 @@ function handleClear() {
       </div>
     </div>
     <div class="h-8 flex justify-between items-center">
-      <a-space>
+      <a-space :size="16">
         <a-checkbox v-model:checked="showTime">显示时间</a-checkbox>
         <a-radio-group v-model:value="showType" size="small">
           <a-radio-button value="Hex">Hex</a-radio-button>
           <a-radio-button value="Ascii">Ascii</a-radio-button>
         </a-radio-group>
+        <a-tooltip>
+          <template #title>注意：蓝色背景(<FallOutlined />)为接收数据，灰色背景( <RiseOutlined />)为发送数据</template>
+          <QuestionCircleOutlined />
+        </a-tooltip>
       </a-space>
       <a-button size="small" @click="handleClear">清空</a-button>
     </div>
