31c466f99bac887ca4cb2bfb80c2d3be7ffd3767
linzili <1983160303@qq.com>
2024-06-14 22:12:46 +0800

    ✨ feat(顶部菜单): 动态生成菜单
    - 根据路由配置动态生成菜单项，使得菜单与路由保持一致。
    - 删除了静态的菜单项
    - 添加eslint允许单个单词名称的组件

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,30 +1,45 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, type RouteRecordRaw } from 'vue-router'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'Layout',
+    component: () => import('@/layout/Layout.vue'),
+    redirect: '/network-tool',
+    children: [
+      {
+        path: '/network-tool',
+        name: 'SerialPort',
+        redirect: '/connection-test',
+        meta: {
+          title: '物联工具'
+        },
+        children: [
+          {
+            path: '/connection-test',
+            name: 'ConnectionTest',
+            component: () => import('@/views/network-tool/network-tool.vue'),
+            meta: {
+              title: '连接测试'
+            }
+          },
+          {
+            path: '/serial-port',
+            name: 'SerialPort',
+            component: () => import('@/views/network-tool/network-tool.vue'),
+            meta: {
+              title: '串口映射'
+            }
+          }
+        ]
+      }
+    ]
+  }
+]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes: [
-    {
-      path: '/',
-      redirect: '/connection-test'
-    },
-    {
-      path: '/',
-      component: (() => import('@/layout/Layout.vue')),
-      children: [
-        {
-          path: '/connection-test',
-          name: '连接测试',
-          component: () => import('@/views/network-tool/network-tool.vue')
-        },
-        {
-          path: '/serial-port',
-          name: '串口映射',
-          component: () => import('@/views/network-tool/network-tool.vue')
-        }
-      ]
-    }
-
-  ]
+  routes
 })
 
 export default router
